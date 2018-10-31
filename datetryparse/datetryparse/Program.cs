2adc0e8d4a5b23a3a1f6f72b6c130cdbb2d5ab8c
kavyasree_r <kavyasree187@gmail.com>
2018-10-31 23:23:04 +0530

    Initial commit :wq :wq

---
+++ datetryparse/datetryparse/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datetryparse
+{
+
+    class Program
+    {       
+            public static int Main()
+            {
+            // int i = 0;
+            
+            string name = Console.ReadLine();
+            string[] arr = name.Split(' ');
+             string[] j;
+                for ( int i = 0; i < arr.Length; i++)
+                {
+                // Console.WriteLine(arr[i]);
+                //Console.WriteLine(arr[i].Length);
+                do
+                {
+                    if (name[i] == ' ')
+                    {
+                        return i;
+
+                    }
+                    else
+                        return 0;
+                }while(name[i]=='\0');
+            }
+            Console.WriteLine("Added tis line for the git);
+            return 0;
+            }
+
+
+       
+
+        }
+    
+}
